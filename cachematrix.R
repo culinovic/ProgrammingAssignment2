0822b01bfe99e4201fcbefbafd76ce8b3ff9efe1
culinovic <culinovic@gmail.com>
2014-11-23 22:57:26 +0100

    my solution to programming asignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        imtx=NULL
+        set=function(y){
+                x<<-y
+                imtx<<-NULL
+        }
+        get=function() x
+        setimtx=function(inverse) imtx  <<- inverse 
+        getimtx=function() imtx 
+        list(set=set,get=get,setimtx=setimtx,getimtx=getimtx)
 }
 
 
@@ -12,4 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        imtx=x$getimtx()
+        if(!is.null(imtx))return(imtx)
+        mat.data=x$get()
+        imtx=solve(mat.data, ...)
+        x$setimtx(imtx)
+        return(imtx)
 }
